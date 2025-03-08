792613681aa6d7830f0929771811579bd3be75cf
srini_11 <srisrinivasan251@gmail.com>
2025-03-09 00:43:45 +0530

    Swagger URL updated

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: "http://localhost:3000",
+        url: "https://event-analytics-z1zd.onrender.com",
       },
     ],
     tags: [
